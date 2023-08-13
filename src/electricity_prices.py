c622f8208cc106bbbdebd5fc662b0e9fad9f1f21
Jan Zahradník <60535859+jenicek001@users.noreply.github.com>
2023-08-13 12:04:24 +0200

    Update electricity_prices.py
    code clearnup, log levels etc.

--- src/electricity_prices.py
+++ src/electricity_prices.py
@@ -3,6 +3,7 @@ import asyncio
 import configparser
 from logger import log
 from datetime import date, datetime, timedelta
+import json
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -16,18 +17,14 @@ class ElectricityPrices:
         self.spot_electricity_price_today = [0.0] * 24
         self.spot_electricity_price_today_valid = True # TODO must be invalidated at midnight
         self.spot_electricity_price_last_update_time = None
-        my_array = [0.0] * 24
  
-    def fetch_prices(self):
-        # Implement the code to fetch hourly spot electricity prices from OTE-CR API
-        # and return the data in CZK
-        pass
-
     async def async_update_spot_electricity_costs(self):
-        log.info("Updating spot electricity costs from OTE-CR")
+        log.info("Fetching spot electricity costs from OTE-CR...")
 
         today_date_local = date.today()
         timestamp_now = datetime.now()
+
+        success = False
 
         try:
             async with aiohttp.ClientSession() as session:
@@ -37,11 +34,11 @@ class ElectricityPrices:
                     #print(resp.status)
                     #print(await resp.text())
 
-                    log.info(f'EnergyRouting: price cost response: "{resp}"')
+                    log.debug(f'OTE price cost response: "{resp}"')
                     result = await resp.json()
                     request_duration = datetime.now() - request_time
-                    log.info(f'EnergyRouting: price cost request duration: "{request_duration}"')
-                    log.info(f'EnergyRouting: price cost result: "{result}"')
+                    log.debug(f'OTE price cost request duration: "{request_duration}"')
+                    log.debug(f'OTE price cost result: "{result}"')
 
                     if 'data' in result:
                         if 'dataLine' in result['data']:
@@ -53,97 +50,33 @@ class ElectricityPrices:
                                             self.spot_electricity_price_today[i] = hourly_spot_price[i]['y']
                                             self.spot_electricity_price_today_valid = True # TODO must be invalidated at midnight
                                             self.spot_electricity_price_last_update_time = timestamp_now
-                                            log.debug(f'EnergyRouting: received daily spot electricity prices from OTE-CR: hour: {i:02d}:00-{(i+1):02d}:00 {self.spot_electricity_price_today[i]} EUR/MWh')
+                                            log.debug(f'OTE price cost received daily spot electricity prices from OTE-CR: hour: {i:02d}:00-{(i+1):02d}:00 {self.spot_electricity_price_today[i]} EUR/MWh')
+                                            success = True
                                         # self.report_hourly_spot_electricity_costs()
                                     else:
-                                        log.error(f'EnergyRouting: expected 24 values for daily spot prices, received {len(hourly_spot_price)}')
+                                        log.error(f'OTE price cost expected 24 values for daily spot prices, received {len(hourly_spot_price)}')
                                 else:
-                                    log.error(f'EnergyRouting: missing "point" array in JSON from OTE-CR')
+                                    log.error(f'OTE price cost missing "point" array in JSON from OTE-CR')
                             else:
-                                log.error(f'EnergyRouting: "dataLine" in JSON from OTE-CR does not have expected 2 items - amount and price')
+                                log.error(f'OTE price cost "dataLine" in JSON from OTE-CR does not have expected 2 items - amount and price')
                         else:
-                            log.error(f'EnergyRouting: missing "dataLine" array in JSON from OTE-CR')
+                            log.error(f'OTE price cost missing "dataLine" array in JSON from OTE-CR')
                     else:
-                        log.error(f'EnergyRouting: missing "data" in JSON from OTE-CR')
+                        log.error(f'OTE price cost missing "data" in JSON from OTE-CR')
 
         except OSError as err:
-            log.error(f'EnergyRouting: error receiving data from OTE-CR: "{err}"')
+            log.error(f'OTE price cost error receiving data from OTE-CR: "{err}"')
 
             if timestamp_now - self.spot_electricity_price_last_update_time > timedelta(hours=24):
-                self.spot_electricity_price_today_valid = False # invalidate if failed update and last value is depreciated
+                return False
 
-    async def async_update_czk_eur_exchange_rates(self):
-        # download rates from CNB from: https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/graph_data.js?currency=EUR&format=json
-        log.info("EnergyRouting: updating daily EUR/CZK exchange rate from CNB")
+        except Exception as err:
+            log.error(f'OTE price cost error receiving data from OTE-CR: "{err}"')
 
-        today_date_local = date.today()
-        # today_date is formatted as string in format YYYY-MM-DD
-        #today_date = today_date_local.strftime("%Y-%m-%d")
+            if timestamp_now - self.spot_electricity_price_last_update_time > timedelta(hours=24):
+                return False
+            
+        return success
 
-        # today_date is formatted as string in format DD.MM.YYYY
-        today_date = today_date_local.strftime("%d.%m.%Y")
-
-        try:
-            # async with self.async_http.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/graph_data.js?currency=EUR&format=json') as resp:
-            #     log.info(f'EnergyRouting: exchange rate response: "{resp}"')
-            #     result = await resp.json()
-            #     log.info(f'EnergyRouting: exchange rate result: "{result}"')
-
-            #     if len(result) > 1:
-            #         if result[-1][0] == today_date:
-            #             self.today_rate = result[-1][1] # last value is for today
-            #             self.today_rate_valid = True
-            #             self.previous_rate = result[-2][1] # second last value is for yesterday or last working day
-            #             self.previous_rate_valid = True
-            #             log.info(f'EnergyRouting: received daily EUR/CZK exchange rate from CNB: {self.today_rate} CZK/EUR')
-            #         else:
-            #             self.today_rate_valid = False
-            #             self.previous_rate = result[-1][1] # last value is for yesterday or last working day
-            #             self.previous_rate_valid = True
-            #             log.info(f'EnergyRouting: received last valid EUR/CZK exchange rate from CNB: {self.previous_rate} CZK/EUR')
-            async with self.async_http.get(f'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date={today_date}') as resp:
-                # log.info(f'EnergyRouting: exchange rate response: "{resp}"')
-                result = await resp.text()
-                # log.info(f'EnergyRouting: exchange rate result: "{result}"')
-                # parse result
-                currencies = result.splitlines()
-                # log.info(f'EnergyRouting: currencies: "{currencies}"')
-                # 1. line: "24.02.2023 #40"
-                # 2. line: "země|měna|množství|kód|kurz"
-                # 3. line: "Austrálie|dolar|1|AUD|15,100"
-                if len(currencies) < 2:
-                    log.error(f'EnergyRouting: error receiving EUR/CZK rate from CNB: invalid response')
-                    self.today_rate_valid = False
-                    self.previous_rate_valid = False
-                    return
-                rate_date = currencies[0].split(" #")[0]
-                log.info(f'EnergyRouting: EUR/CZK rate date: "{rate_date}"')
-                self.today_rate_valid = False
-
-                for currency in currencies[2:]:
-                    # log.info(f'EnergyRouting: currency: "{currency}"')
-                    if currency == "": # skip empty lines
-                        continue
-                    currency_data = currency.split("|")
-                    # log.info(f'EnergyRouting: currency_data: "{currency_data}"')
-                    if len(currency_data) == 5 and currency_data[3] == "EUR":
-                        if rate_date == today_date:
-                            self.today_rate = float(currency_data[4].replace(",", "."))
-                            self.today_rate_valid = True
-                            log.info(f'EnergyRouting: received daily EUR/CZK exchange rate from CNB: {self.today_rate} CZK/EUR')
-                            self.openhab_current_czk_to_eur_rate_today_item.oh_send_command(self.today_rate)
-                        else:
-                            self.previous_rate = float(currency_data[4].replace(",", "."))
-                            self.previous_rate_valid = True
-                            self.today_rate_valid = False
-                            log.info(f'EnergyRouting: received last valid EUR/CZK exchange rate from CNB: {self.previous_rate} CZK/EUR')
-                            self.openhab_current_czk_to_eur_rate_today_previous_item.oh_send_command(self.previous_rate)
-                        break
-                
-               
-        except OSError as err:
-            log.error(f'EnergyRouting: error receiving EUR/CZK rate from CNB: "{err}"')
-            self.today_rate_valid = False
-            self.previous_rate_valid = False
-        # download today and yesterday rates
-        # https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=12.11.2020
+    def get_current_price(self):
+        return self.spot_electricity_price_today[datetime.now().hour]
