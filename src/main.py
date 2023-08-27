3787fc849f812b3c4d55611bd41eec41ec607a09
Jan Zahradník <60535859+jenicek001@users.noreply.github.com>
2023-08-27 12:31:28 +0200

    Update main.py
    async update

--- src/main.py
+++ src/main.py
@@ -5,40 +5,78 @@ from mqtt_client import MQTTClient
 import asyncio
 import time
 
-async def main():
-    
-    electricity_prices = ElectricityPrices()
-    exchange_rate = ExchangeRate()
-    mqtt_client = MQTTClient()
 
-    while True:
-        try:
-            # Fetch electricity prices and exchange rate
-            #electricity_price_czk = electricity_prices.fetch_prices()
-            exchange_rate_czk_eur = await exchange_rate.async_update_exchange_rate()
-            log.info(f"Exchange rate: {exchange_rate_czk_eur} CZK/EUR")
+electricity_prices = ElectricityPrices()
+exchange_rate = ExchangeRate()
+mqtt_client = MQTTClient()
 
-            if exchange_rate_czk_eur is not None:
+async def main_callback():
+    log.info("main_callback...")
+    try:
+        # Fetch electricity prices and exchange rate
+        #electricity_price_czk = electricity_prices.fetch_prices()
+        exchange_rate_czk_eur = await exchange_rate.async_update_exchange_rate()
+        log.info(f"Exchange rate: {exchange_rate_czk_eur} CZK/EUR")
 
-                result = await electricity_prices.async_update_spot_electricity_costs()
-                if result:
-                    electricity_price_eur = electricity_prices.get_current_price() # in EUR
-                    electricity_price_czk = electricity_price_eur * exchange_rate_czk_eur / 1000.0
-                    log.info(f"Electricity price 1kWh: {electricity_price_czk} CZK")
+        if exchange_rate_czk_eur is not None:
 
-                    # Publish the price in CZK via MQTT
-                    mqtt_client.publish_price(electricity_price_czk)
-                else:
-                    log.error("Failed to fetch electricity prices from OTE-ČR")
+            result = await electricity_prices.async_update_spot_electricity_costs()
+            if result:
+                electricity_price_eur = electricity_prices.get_current_price() # in EUR
+                electricity_price_czk = electricity_price_eur * exchange_rate_czk_eur / 1000.0
+                log.info(f"Electricity price 1kWh: {electricity_price_czk} CZK")
+
+                # Publish the price in CZK via MQTT
+                mqtt_client.publish_price(electricity_price_czk)
             else:
-                log.error("Failed to fetch exchange rate from ČNB")
+                log.error("Failed to fetch electricity prices from OTE-ČR")
+        else:
+            log.error("Failed to fetch exchange rate from ČNB")
 
-            time.sleep(3600)  # Sniff hourly prices (wait for 1 hour)
+    except KeyboardInterrupt:
+        log.info("Stopping the application...")
 
-        except KeyboardInterrupt:
-            break
+# async def main():
+    
+#     electricity_prices = ElectricityPrices()
+#     exchange_rate = ExchangeRate()
+#     mqtt_client = MQTTClient()
+
+#     while True:
+#         try:
+#             # Fetch electricity prices and exchange rate
+#             #electricity_price_czk = electricity_prices.fetch_prices()
+#             exchange_rate_czk_eur = await exchange_rate.async_update_exchange_rate()
+#             log.info(f"Exchange rate: {exchange_rate_czk_eur} CZK/EUR")
+
+#             if exchange_rate_czk_eur is not None:
+
+#                 result = await electricity_prices.async_update_spot_electricity_costs()
+#                 if result:
+#                     electricity_price_eur = electricity_prices.get_current_price() # in EUR
+#                     electricity_price_czk = electricity_price_eur * exchange_rate_czk_eur / 1000.0
+#                     log.info(f"Electricity price 1kWh: {electricity_price_czk} CZK")
+
+#                     # Publish the price in CZK via MQTT
+#                     mqtt_client.publish_price(electricity_price_czk)
+#                 else:
+#                     log.error("Failed to fetch electricity prices from OTE-ČR")
+#             else:
+#                 log.error("Failed to fetch exchange rate from ČNB")
+
+#             time.sleep(3600)  # Sniff hourly prices (wait for 1 hour)
+
+#         except KeyboardInterrupt:
+#             break
 
 if __name__ == "__main__":
     log.info("Starting the application...")
 
-    asyncio.run(main())
+    #asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.call_soon(callback=main_callback)
+    # Blocking call interrupted by loop.stop()
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
