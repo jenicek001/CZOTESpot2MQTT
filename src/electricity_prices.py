6e93ba6f1e21cf3fac9ac9f5e9625271aa7eb03e
Jan Zahradník <60535859+jenicek001@users.noreply.github.com>
2023-08-27 12:31:17 +0200

    Update electricity_prices.py
    minor init changes

--- src/electricity_prices.py
+++ src/electricity_prices.py
@@ -14,10 +14,16 @@ class ElectricityPrices:
     def __init__(self):
         self.api_key = api_key
         self.url = "https://api.example.com/ote-cr-api/v1/hourly_spot_prices"
-        self.spot_electricity_price_today = [0.0] * 24
-        self.spot_electricity_price_today_valid = True # TODO must be invalidated at midnight
-        self.spot_electricity_price_last_update_time = None
- 
+        self.hourly_spot_electricity_price = [0.0] * 24
+        self.hourly_spot_electricity_price_last_update_time = None
+
+    def is_hourly_spot_electricity_price_valid(self):
+        if self.hourly_spot_electricity_price_last_update_time is None:
+            return False
+        if datetime.now() - self.hourly_spot_electricity_price_last_update_time > timedelta(hours=1):
+            return False
+        return True
+
     async def async_update_spot_electricity_costs(self):
         log.info("Fetching spot electricity costs from OTE-CR...")
 
@@ -30,10 +36,7 @@ class ElectricityPrices:
             async with aiohttp.ClientSession() as session:
                 request_time = datetime.now()
                 async with session.get(f'https://www.ote-cr.cz/cs/kratkodobe-trhy/elektrina/denni-trh/@@chart-data?report_date={today_date_local}') as resp:
-                    log.debug(f'get: {resp.status}')
-                    #print(resp.status)
-                    #print(await resp.text())
-
+                    log.debug(f'OTE price cost GET: {resp.status}')
                     log.debug(f'OTE price cost response: "{resp}"')
                     result = await resp.json()
                     request_duration = datetime.now() - request_time
@@ -47,12 +50,10 @@ class ElectricityPrices:
                                     hourly_spot_price = result['data']['dataLine'][1]['point']
                                     if len(hourly_spot_price) == 24:
                                         for i in range(0, 24):
-                                            self.spot_electricity_price_today[i] = hourly_spot_price[i]['y']
-                                            self.spot_electricity_price_today_valid = True # TODO must be invalidated at midnight
-                                            self.spot_electricity_price_last_update_time = timestamp_now
-                                            log.debug(f'OTE price cost received daily spot electricity prices from OTE-CR: hour: {i:02d}:00-{(i+1):02d}:00 {self.spot_electricity_price_today[i]} EUR/MWh')
+                                            self.hourly_spot_electricity_price[i] = hourly_spot_price[i]['y']
+                                            self.hourly_spot_electricity_price_last_update_time = timestamp_now
+                                            log.debug(f'OTE price cost received daily spot electricity prices from OTE-CR: hour: {i:02d}:00-{(i+1):02d}:00 {self.hourly_spot_electricity_price[i]} EUR/MWh')
                                             success = True
-                                        # self.report_hourly_spot_electricity_costs()
                                     else:
                                         log.error(f'OTE price cost expected 24 values for daily spot prices, received {len(hourly_spot_price)}')
                                 else:
@@ -66,17 +67,13 @@ class ElectricityPrices:
 
         except OSError as err:
             log.error(f'OTE price cost error receiving data from OTE-CR: "{err}"')
-
-            if timestamp_now - self.spot_electricity_price_last_update_time > timedelta(hours=24):
-                return False
+            success = False
 
         except Exception as err:
             log.error(f'OTE price cost error receiving data from OTE-CR: "{err}"')
-
-            if timestamp_now - self.spot_electricity_price_last_update_time > timedelta(hours=24):
-                return False
+            success = False
             
         return success
 
     def get_current_price(self):
-        return self.spot_electricity_price_today[datetime.now().hour]
+        return self.hourly_spot_electricity_price[datetime.now().hour]
